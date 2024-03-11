bea56806ce8b8a7018f0aea74430e838de4057d6
Njb1286 <bennionnj@gmail.com>
2024-03-10 21:28:36 -0600

    Constrained the image type in the upload form

--- public/assets/CardBody-28359e74.js
+++
@@ -1,1 +0,0 @@
-import{r as e,u as c,j as m,c as p}from"./index-0fc6879d.js";const s=e.forwardRef(({className:r,bsPrefix:a,as:o="div",...d},t)=>(a=c(a,"card-body"),m.jsx(o,{ref:t,className:p(r,a),...d})));s.displayName="CardBody";const n=s;export{n as C};
